f67dd57c0b2f925e207494cd149e669d7110c148
ManasaY13 <64654749+ManasaY13@users.noreply.github.com>
2021-07-11 16:08:15 +0530

    Add files via upload

---
+++ users.js
@@ -0,0 +1,18 @@
+const users = [
+  {
+    "userId": 1,
+    "name": "Manasa",
+    "age": 20
+  },
+  {
+    "userId": 2,
+    "name": "Nam",
+    "age": 21
+  },
+  {
+    "userId": 3,
+    "name": "seshankitha",
+    "age": 22
+  }
+]
+module.exports = users;
